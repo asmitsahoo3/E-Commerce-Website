4e98b03c37b4ece0587abbace705b0606da0dd93
asmitsahoo3 <asmitsahoo2002@gmail.com>
2024-12-30 22:56:39 +0530

    E-commerce Website

---
+++ index.js
@@ -0,0 +1,15 @@
+//alert("You are always Welcome to have a Great Experience");
+
+const bar=document.getElementById('bar');
+const close=document.getElementById('close');
+const nav=document.getElementById('navbar');
+if(bar){
+    bar.addEventListener('click',()=>{
+        nav.classList(active);
+    })
+}
+if(close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove(active);
+    })
+}
